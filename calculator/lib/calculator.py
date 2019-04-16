8fc90c6b130e833f588d3ce15c7c3a97794f39f6
hpfn-d <terberh@gmail.com>
2019-04-16 11:55:35 -0300

    add division feature, test for it

--- calculator/lib/calculator.py
+++ calculator/lib/calculator.py
@@ -13,3 +13,8 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def division(first_term, second_term):
+    """ integer - // """
+    return first_term // second_term
--- calculator/tests/test_calculator.py
+++ calculator/tests/test_calculator.py
@@ -2,7 +2,7 @@
 Unit tests for the calculator library
 """
 
-from calculator.lib.calculator import add, subtract, multiply
+from calculator.lib.calculator import add, division, subtract, multiply
 
 
 class TestCalculator:
@@ -15,3 +15,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == multiply(10, 10)
+
+    def test_division(self):
+        assert 10 == division(100, 10)
