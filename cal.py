d3dc7a21f4b6cd0dc945fa5df87d9017f05dc94e
hpfn-d <terberh@gmail.com>
2019-04-16 11:30:43 -0300

    project tree and tests

---
+++ cal.py
@@ -0,0 +1,7 @@
+from calculator.lib.calculator import add
+
+x = 1
+y = 3
+r = add(x, y)
+
+print(r)
---
+++ calculator/tests/test_calculator.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for the calculator library
+"""
+
+from calculator.lib.calculator import add, subtract
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == subtract(4, 2)
